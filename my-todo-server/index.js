b2d457cf94e5bfa810ee578e81cff0171a04a64c
Arafin Ahmed <arafin2021@gmail.com>
2021-03-25 13:12:40 +0600

    done

--- my-todo-server/index.js
+++ my-todo-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port = 8888;
@@ -9,9 +10,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+const uri = `mongodb+srv://organicUser:qMtIUh2zIHymXuTP@cluster0.2jkon.mongodb.net/Tasks?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 app.get('/', (req, res) => {
     res.send("This is working");
 })
+
+
+client.connect(err => {
+  const todosCollection = client.db("Tasks").collection("todos");
+  console.log("connected");
+
+  app.post('/createTodo', (req, res) => {
+      const myTask = req.body;
+      todosCollection.insertOne(myTask)
+      .then(result => {
+          if(result.insertedCount>0){
+              res.send({"message": "Data added successfully."});
+          }
+          else{
+            res.send({"message": "Failed to add data."});
+          }
+      })
+      
+  })
+
+//   client.close();
+});
+
 
 app.listen(port, () =>{
     console.log("Listening to port", port);
--- my-todo/src/components/Input/Input.js
+++ my-todo/src/components/Input/Input.js
@@ -3,8 +3,17 @@ import React from 'react';
 const Input = () => {
     const onSubmit = (e) => {
         console.log("on submit clicked");
-        console.log(e.target.inputText.value);
-        console.log(e.target.deadline.value);
+        const task = e.target.inputText.value;
+        const deadline = e.target.deadline.value;
+        const active = true;
+        const data = {task, deadline, active}
+        fetch('http://localhost:8888/createTodo', {
+            method:'POST',
+            body:JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(res => res.json())
+        .then(data => console.log(data.message))
         e.target.inputText.value = "";
         e.target.deadline.value = "";
         e.preventDefault();
